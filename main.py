7946c114d6ba9a86a72e4802072682f4a923b3bc
kanshi <shinker441@gmail.com>
2024-03-08 20:17:20 +0900

    message test3 commit

--- main.py
+++ main.py
@@ -1,121 +1,54 @@
 import os
-import json
-import re
-import requests
-import asyncio
-import aiohttp
-from fastapi import FastAPI, Query, Header, Request
-from fastapi.responses import FileResponse
-from fastapi.middleware.cors import CORSMiddleware
-from dotenv import load_dotenv
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.responses import JSONResponse
 from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent
-from starlette.exceptions import HTTPException
+from linebot.exceptions import InvalidSignatureError, LineBotApiError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
-load_dotenv()
-# FastAPIのインスタンス作成
+# 環境変数からLINE Messaging APIの設定値を取得
+LINE_CHANNEL_ACCESS_TOKEN = os.getenv("2003969131")
+LINE_CHANNEL_SECRET = os.getenv("2360bd36b3c2e5a794e0834b4ddd5fc2")
+
 app = FastAPI()
 
-# LINE Botに関するインスタンス作成
-line_bot_api = LineBotApi("2003969131")
-handler = WebhookHandler("2360bd36b3c2e5a794e0834b4ddd5fc2")
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
+# LINE Bot APIクライアントのインスタンス化
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
 @app.get("/")
 def root():
-    return {"title": 'hello world'}
-
+    return {"title": "Hello World"}
 
 @app.post("/webhook")
-async def line_webhook(message: str = Query(None)):  # Queryのデフォルト値をNoneに設定
-    print(message)
-    global A
+async def line_webhook(request: Request):
+    # リクエストボディと署名を取得
+    body = await request.body()
+    signature = request.headers.get('X-Line-Signature')
 
-    if message == "こんにちは":
-        A = {
-            "replyToken": json.loads(message['body']['events'][0]['replyToken']),
-            "message": [
-                {
-                    "type": "text",
-                    "text": "hello",
-                }
-            ]
-        }
-    elif message == "こんばんわ":
-        A = {
-            "replyToken": json.loads(message['body']['events'][0]['replyToken']),
-            "message": [
-                {
-                    "type": "text",
-                    "text": "good night",
-                }
-            ]
-        }
+    # 署名の検証とイベントハンドラの呼び出し
+    try:
+        handler.handle(body.decode('utf-8'), signature)
+    except InvalidSignatureError:
+        raise HTTPException(status_code=400, detail="Invalid signature")
+    except LineBotApiError as e:
+        raise HTTPException(status_code=500, detail=f"Line Bot API error: {e}")
+    
+    return JSONResponse(content={"status": "success"})
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_text_message(event):
+    # ユーザーからのテキストメッセージに基づいて応答
+    user_message = event.message.text
+
+    if user_message == "こんにちは":
+        response_message = "こんにちは！いかがお過ごしですか？"
+    elif user_message == "こんばんは":
+        response_message = "こんばんは！素敵な夜をお過ごしください。"
     else:
-        A = {
-            "replyToken": json.loads(message['body']['events'][0]['replyToken']),
-            "message": [
-                {
-                    "type": "text",
-                    "text": "hello world"
-                }
-            ]
-        }
-    return A
+        response_message = "申し訳ありません、よく理解できませんでした。"
 
-'''
-
-    if message == "こんにちは":
-        res_data = "message:こんにちは！ようこそ"
-        print(res_data)
-        return res_data
-
-    elif message == "質問してもいいですか":
-        res_data = "message:もちろんです。何かお困りですか？"
-        print(res_data)
-        return res_data
-    else:
-        res_data = "すいません。よくわかりません。"
-        print(res_data)
-        return res_data
-'''
-
-
-@handler.add(MessageEvent)
-def handle_message(event):
-    line_bot_api.reply_message(event.reply_token, A)
-
-# async def send_request():
-#     while True:
-#         async with aiohttp.ClientSession(connector=connector) as session:
-#             async with session.get(deploy_url) as response:
-#                 print(await response.text())
-#         await asyncio.sleep(50)  # 50秒ごとにリクエストを送信
-
-
-# @app.on_event("startup")
-# async def startup_event():
-#     asyncio.create_task(send_request())
-
-
-'''
-------------------------------------------------------------------------------------------------------
-@app.get(
-    "/resources/{directory}/{file}",
-    summary="静的ファイルの取得用です。",
-    description="指定したパスのファイルをレスポンスします。",
-)
-def resources(directory: str, file: str):
-
-    return FileResponse("./app/resources/" + directory + "/" + file)
-------------------------------------------------------------------------------------------------------
-'''
+    # 応答メッセージの送信
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=response_message)
+    )
